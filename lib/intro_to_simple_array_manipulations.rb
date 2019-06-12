42713446656aff763915aeadb7641cc1fa0ccb51
Adrian Popan <adrian.popan@gmail.com>
2019-06-12 15:51:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,8 +4,9 @@ def using_push(array, string)
   array.push(string)
 end 
 
-
+# takes in two arguments, an array and a string and adds that string to the front of the array using the unshift method
 
 def using_unshift(array, string)
   array.unshift(string)
 end 
+
